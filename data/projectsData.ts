3a3ab2b5c56c9f762f7cb6a393fbb49532706e99
hsx <hanxiang@sightp.com>
2024-03-04 16:45:06 +0800

    blog: docker, linux, git

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,21 +7,11 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'CV Matcher',
+    description: `使用opencv 对图片相似度进行匹配，server模块提供restful规范接口与前端通信。`,
     imgSrc: '/static/images/google.png',
     href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+  }
 ]
 
 export default projectsData
