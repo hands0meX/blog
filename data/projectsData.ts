c805792983be565588fb9e302ecd6d8ccd851311
hands0meX <1258849260@qq.com>
2024-03-04 17:42:49 +0800

    blog: docker build & project cvmatcher

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,8 +9,8 @@ const projectsData: Project[] = [
   {
     title: 'CV Matcher',
     description: `使用opencv 对图片相似度进行匹配，server模块提供restful规范接口与前端通信。`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    imgSrc: '/static/images/matches.jpg',
+    href: 'https://github.com/dawnapproaching/matcher',
   }
 ]
 
